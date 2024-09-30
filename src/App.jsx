c245fb15d68ce63879fa882ec658a279399b797d
Camille Webb <camillewebb35@gmail.com>
2024-09-30 17:34:15 -0600

    made the page more dynamic and added some styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,30 @@ import { useState } from 'react';
 function App() {
 
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupID, setFeatPupID] = useState(null);
 
-    console.log("puppyList: ", puppyList);
+  const featuredPup = puppies.find((pup) => pup.id === featPupID);
+  console.log(featuredPup);
+  
   
   return (
     <>
       <div className="App">
         {
           puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
+            return <p onClick={() => { setFeatPupID(puppy.id);
+            }} key={puppy.id}>{puppy.name}</p>
           })
         }
+        { featPupID && (
+          <div className="Featured-Pup">
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
       </div>
     </>
   );
