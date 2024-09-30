07d265be46554bdc210b44b21dc353b4133996c4
Camille Webb <camillewebb35@gmail.com>
2024-09-30 17:04:09 -0600

    rendered list of puppy names

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import "./App.css";
+import { puppyList } from "./data.js";
+import { useState } from 'react';
 
 function App() {
+
+  const [puppies, setPuppies] = useState(puppyList);
+
+    console.log("puppyList: ", puppyList);
+  
   return (
     <>
-      <div></div>
+      <div className="App">
+        {
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>
+          })
+        }
+      </div>
     </>
   );
 }
